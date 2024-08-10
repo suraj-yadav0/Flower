56fe3af6947d073aad423f0f2e0a34aeb0aaff1f
Suraj Yadav <surajyadav200701@gmail.com>
2024-08-11 01:42:49 +0530

    added the flower on HomeSxreen

--- flower/lib/home_screen.dart
+++ flower/lib/home_screen.dart
@@ -1,1 +1,21 @@
-st
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:lottie/lottie.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body:  SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: LottieBuilder.asset("flower/assets/FLOWER.json"),
+    ),);
+  }
+}
--- flower/lib/main.dart
+++ flower/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flower/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
